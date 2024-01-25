a55a567f95cbd3ea5b867f77d93a5be7349e59dd
Adrián Castelló <adcastel@disca.upv.es>
2024-01-25 12:49:21 +0100

    New format

--- main.py
+++ main.py
@@ -16,16 +16,18 @@ def main(d, h, l, b):
     print("Starting the Transformer Layer Simulator")
     print("D = {}, H = {}, L={}, B = {}".format(d, h, l, b))
     f = 4 * d
+    assert (b  == 1)
     l = l * b
     ######################### Initial data
     EI = np.random.rand(d, l)
     Wq = np.random.rand(d, d)
     Wk = np.random.rand(d, d)
     Wv = np.random.rand(d, d)
-    Wout = np.random.rand(d, d)
+    Wo = np.random.rand(d, d)
 
     W1 = np.random.rand(f, d)
     W2 = np.random.rand(d, f)
+
     runing_mean = np.random.rand(l)
     inv_std = np.random.rand(l)
     gamma = np.random.rand(l)
@@ -38,45 +40,47 @@ def main(d, h, l, b):
     E1s = []
     E2s = []
     ##########################
-    Q = gemm(Wq, EI)
-    K = gemm(Wk, EI)
-    V = gemm(Wv, EI)
-
+    Q = gemm(Wq, EI)  #MHA 1)
+    K = gemm(Wk, EI)  #MHA 2)
+    V = gemm(Wv, EI)  #MHA 3)
     assert (Q.shape == K.shape == V.shape == (d, l))
 
-    for i in range(split):
-        E1s.append(gemm(K[i * h:(i + 1) * h, 0:l], Q[i * h:(i + 1) * h, 0:l], transA=True))
+    for i in range(split):  #MHA 4)
+        E1s.append(gemm(K[i * h:(i + 1) * h, 0:l],
+                        Q[i * h:(i + 1) * h, 0:l],
+                        transA=True)) #MHA 5)
         assert (E1s[i].shape == (l, l))
-        E1s[i] = softmax(E1s[i])
 
-        E2s.append(gemm(V[i * h:(i + 1) * h, 0:l], E1s[i]))
+        E1s[i] = softmax(E1s[i]) #MHA 6)
+
+        E2s.append(gemm(V[i * h:(i + 1) * h, 0:l], E1s[i])) #MHA 7)
         assert (E2s[i].shape == (h, l))
 
     print(E2.shape)
-    for i in range(split):
+    for i in range(split):        #MHA 8)
         E2[i * h:(i + 1) * h, 0:l] = E2s[i]
-    #
+
     assert (E2.shape == (d, l))
-    AO = gemm(Wout, E2)
-    # AO layer norm (A0) + EI
+
+    AO = gemm(Wo, E2)             #MHA 9)
     assert (AO.shape == (d, l))
 
-    #AO = bn_inference_cython(AO, runing_mean, inv_std, gamma, beta)
-    AO = AO + EI
+
+    # AO layer norm (AO + EI)    #MHA 10)
     assert (AO.shape == (d, l))
 
     ########### FFN
 
-    E4 = gemm(W1, AO)
-
+    E4 = gemm(W1, AO)            #FFN 11)
     assert (E4.shape == (f, l))
 
-    # E4 = gelu(E4)
+    # E4 = gelu(E4)             #FFN 12)
 
-    E0 = gemm(W2, E4) + AO
-    # E0 layer norm (E0) + AO
-    print(E0)
-    assert (E0.shape == (d, l))
+    EO = gemm(W2, E4)           #FFN 13)
+    # EO layer norm (EO + AO)   #FFN 14)
+    assert (EO.shape == (d, l))
+
+    print(EO)
 
 
 # Press the green button in the gutter to run the script.
